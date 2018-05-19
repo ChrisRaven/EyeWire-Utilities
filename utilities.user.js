82b46b89b96d8ef4d4b6a7dca4f5b998f1ac7171
Krzysztof Kruk <kr.kruk@gmail.com>
2018-05-19 02:27:02 +0200

    Shift+click to copy child ID to clipboard

--- utilities.user.js
+++ utilities.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Utilities
 // @namespace    http://tampermonkey.net/
-// @version      1.8
+// @version      1.8.1
 // @description  Utilities for EyeWire
 // @author       Krzysztof Kruk
 // @match        https://*.eyewire.org/*
@@ -777,6 +777,10 @@ if (LOCAL) {
   }
 
   document.addEventListener('dblclick', function (event) {
+    if (!settings.getValue('show-childrens-ids')) {
+      return;
+    }
+
     let world = tomni.threeD.getWorld();
     let container = document.getElementById('threeD');
     let array = getMousePosition(container, event.clientX, event.clientY);
@@ -793,6 +797,81 @@ if (LOCAL) {
       }
     }
   }, false);
+
+
+  // source: https://stackoverflow.com/a/30810322
+  function copyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+  
+    //
+    // *** This styling is an extra step which is likely not required. ***
+    //
+    // Why is it here? To ensure:
+    // 1. the element is able to have focus and selection.
+    // 2. if element was to flash render it has minimal visual impact.
+    // 3. less flakyness with selection and copying which **might** occur if
+    //    the textarea element is not visible.
+    //
+    // The likelihood is the element won't even render, not even a flash,
+    // so some of these are just precautions. However in IE the element
+    // is visible whilst the popup box asking the user for permission for
+    // the web page to copy to the clipboard.
+    //
+  
+    // Place in top-left corner of screen regardless of scroll position.
+    textArea.style.position = 'fixed';
+    textArea.style.top = 0;
+    textArea.style.left = 0;
+  
+    // Ensure it has a small width and height. Setting to 1px / 1em
+    // doesn't work as this gives a negative w/h on some browsers.
+    textArea.style.width = '2em';
+    textArea.style.height = '2em';
+  
+    // We don't need padding, reducing the size if it does flash render.
+    textArea.style.padding = 0;
+  
+    // Clean up any borders.
+    textArea.style.border = 'none';
+    textArea.style.outline = 'none';
+    textArea.style.boxShadow = 'none';
+  
+    // Avoid flash of white box if rendered for any reason.
+    textArea.style.background = 'transparent';
+  
+  
+    textArea.value = text;
+  
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    var successful = document.execCommand('copy');
+
+    document.body.removeChild(textArea);
+  }
+
+  $(document).click(function (event) {
+    if (!settings || !settings.getValue('show-childrens-ids')) {
+      return;
+    }
+
+    if (!event.shiftKey) {
+      return;
+    }
+
+    let world = tomni.threeD.getWorld();
+    let container = document.getElementById('threeD');
+    let array = getMousePosition(container, event.clientX, event.clientY);
+    onClickPosition.fromArray(array);
+    let neighbours = world.getObjectByName('neighbours');
+    if (neighbours) {
+      let intersects = getIntersects(onClickPosition, neighbours.children);
+      if (intersects.length) {
+        copyTextToClipboard(intersects[0].object.name);
+      }
+    }
+  });
 
   // "cube-enter-triggered", because when jumping between relatives, the "cube-leave-triggered" isn't triggered
   $(document).on('cube-enter-triggered.utilities cube-leave-triggered.utilities', function () {
